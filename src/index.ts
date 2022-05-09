6a47396f7b9361449e0c66e93eb48a46968b1a74
dimitar <dimitar.bochvarovski@sia-soft.com>
2022-05-09 16:24:59 +0200

    Save Bored Apes NFT  Holders to json file and read the holders count from file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import fs from "fs"
 import Web3 from "web3"
 
 import dotenv from "dotenv"
@@ -34,27 +35,59 @@ const rollbotsTransferEvent = ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a1
 
 
 async function main() {
+    interface Holders {
+        [holder: string]: string[];
+    }
+
+    let holders: Holders = {}
+
     /*================== BORED APES ======================*/
     let boredApesTransfers = await verifiedContractTransfers(boredApes, BORED_APES_ADDRESS, 'Transfer')
     boredApesTransfers.forEach((transfer: any) => {
-        console.log(`Bored Ape NFT ${transfer.returnValues.tokenId} transferred ${transfer.returnValues.from} -> ${transfer.returnValues.to}, Txn Hash: ${transfer.transactionHash}`)
+        if(!holders[transfer.returnValues.to]) {
+            holders[transfer.returnValues.to] = []
+        }
+        // Remove the NFT from the previous holder
+        if(holders[transfer.returnValues.from]) {
+            let index = holders[transfer.returnValues.from].indexOf(transfer.returnValues.tokenId)
+            holders[transfer.returnValues.from].splice(index, 1)
+           if(holders[transfer.returnValues.from].length == 0) delete holders[transfer.returnValues.from]
+        }
+        // Add the NFT to the current holder
+        holders[transfer.returnValues.to].push(transfer.returnValues.tokenId)
     })
+
+    let data = JSON.stringify(holders, null, 2);
+
+    fs.writeFile('boredApesHolders.json', data, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    })
+
+    console.time("timer1");
+    fs.readFile('boredApesHolders.json', (err, data) => {
+        if (err) throw err
+        let holdersFromFile = JSON.parse(data.toString())
+
+        console.log(Object.keys(holdersFromFile).length);
+        console.timeEnd("timer1");
+    });
 
     /*================== GUTTER CATS ======================*/
-    let gutterCatGangTransfers = await verifiedContractTransfers(gutterCatGang, GUTTER_CATS_GANG_ADDRESS, 'TransferSingle')
-    gutterCatGangTransfers.forEach((transfer: any) => {
-        console.log(`Gutter Cat NFT ${transfer.returnValues.id} transferred ${transfer.returnValues.from} -> ${transfer.returnValues.to}, Quantity: ${transfer.returnValues.value}, Txn Hash: ${transfer.transactionHash}`)
-    })
+    // let gutterCatGangTransfers = await verifiedContractTransfers(gutterCatGang, GUTTER_CATS_GANG_ADDRESS, 'TransferSingle')
+    // gutterCatGangTransfers.forEach((transfer: any) => {
+        // console.log(`Gutter Cat NFT ${transfer.returnValues.id} transferred ${transfer.returnValues.from} -> ${transfer.returnValues.to}, Quantity: ${transfer.returnValues.value}, Txn Hash: ${transfer.transactionHash}`)
+    // })
 
     /*================== ROLLBOTS ======================*/
-    let rollbotsTransfers = await unverifiedContractTransfers(web3, ROLLBOTS_ADDRESS, rollbotsTransferEvent)
-    rollbotsTransfers.forEach((transfer: any) => {
-        let from = transfer.topics[1].slice(0, 2) + transfer.topics[1].slice(26)
-        let to = transfer.topics[2].slice(0, 2) + transfer.topics[2].slice(26)
-        let tokenID = web3.utils.hexToNumber(transfer.topics[3])
-
-        console.log(`Rollbot NFT ${tokenID} transferred ${from} -> ${to}, Txn Hash: ${transfer.transactionHash}`)
-    })
+    // let rollbotsTransfers = await unverifiedContractTransfers(web3, ROLLBOTS_ADDRESS, rollbotsTransferEvent)
+    // rollbotsTransfers.forEach((transfer: any) => {
+        // let from = transfer.topics[1].slice(0, 2) + transfer.topics[1].slice(26)
+        // let to = transfer.topics[2].slice(0, 2) + transfer.topics[2].slice(26)
+        // let tokenID = web3.utils.hexToNumber(transfer.topics[3])
+// 
+        // console.log(`Rollbot NFT ${tokenID} transferred ${from} -> ${to}, Txn Hash: ${transfer.transactionHash}`)
+    // })
 }
 
 main().catch((e) => {
